99ec2b67df3418113ed8df2ddbdaa579e234bc2f
nassim <nassim.tahnint@groupe-leaderinsurance.fr>
2020-04-13 13:55:32 +0200

    continue with template home

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'screens/home.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Portfolio',
-      home: MyStateFulWidget(),
-    );
-  }
-}
-
-class MyStateFulWidget extends StatefulWidget {
-  MyStateFulWidget({Key key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStateFulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Accueil',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Experiences',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: Formations',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 3: Projets',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('BottomNavigationBar Sample'),
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Accueil'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            title: Text('Experiences'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            title: Text('Formations'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            title: Text('Projets'),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
+      home: HomeWidget(),
     );
   }
 }
